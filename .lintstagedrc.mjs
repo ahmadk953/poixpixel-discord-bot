04945bfebdb5ca21dd1028e700bcef6d0b802624
ahmadk953 <103906421+ahmadk953@users.noreply.github.com>
2025-06-16 21:44:18 -0400

    chore: update formating

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,8 +1,14 @@
 import path from 'path';
 import process from 'process';
 
-const buildEslintCommand = (filenames) =>
-  `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+const buildEslintCommand = (filenames) => {
+  // only lint files under src/
+  const srcFiles = filenames.filter((f) => f.startsWith('src/'));
+  if (srcFiles.length === 0) return '';
+  return `eslint ${srcFiles
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+};
 
 const prettierCommand = 'prettier --write';
 
